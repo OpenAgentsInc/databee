047734ff3d494a067dbade1a31a1ede5a962d724
Christopher David <chris@arcadelabs.co>
2023-11-01 14:59:21 -0400

    set up topic test and function

---
+++ databee/topics.py
@@ -0,0 +1,9 @@
+"""Utilities for topic modeling"""
+
+def extract_topics_from_documents(documents):
+    """
+    Extract topics from a list of documents
+    """
+
+
+    return documents
--- tests/test_topic.py
+++ tests/test_topic.py
@@ -1,10 +1,14 @@
 """Testing topic modeling"""
 
 from databee.ingest import ingest_pdf
+from databee.topics import extract_topics_from_documents
 
 def test_topic_modeling():
     """Test return topics from PDF"""
     pages = ingest_pdf("docs/Seasteading-Implementation-Plan.pdf")
     print("-------")
     print(pages[9])
+    print("-------")
+    topics = extract_topics_from_documents(pages)
+    print(topics)
     print("-------")
